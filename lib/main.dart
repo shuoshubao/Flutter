f88ef1e0ade9b7f1b955dc1d0051e04b2d733477
shuoshubao <shuoshubao@gmail.com>
2023-08-23 19:47:09 +0800

    feat: Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,80 +13,68 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        ),
+        title: 'Text',
         home: MyHomePage(),
       ),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var list = [WordPair.random().asLowerCase];
-
-  void handleAddItem() {
-    list.add(WordPair.random().asLowerCase);
-    notifyListeners();
-  }
-
-  void handleRemoveItem() {
-    if (list.isNotEmpty) {
-      list.removeLast();
-      notifyListeners();
-    }
-  }
-}
+class MyAppState extends ChangeNotifier {}
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    print(appState.list);
-
     return Scaffold(
         body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: List.generate(appState.list.length, (i) => i).map((i) {
-              var value = appState.list[i];
-              return Row(children: [
-                Text(
-                  (i).toString(),
-                  style: TextStyle(color: Colors.cyan, fontSize: 16),
-                ),
-                SizedBox(
-                  width: 8,
-                ),
-                Text(
-                  value,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(color: Colors.purple, fontSize: 20, fontWeight: FontWeight.bold),
-                )
-              ]);
-            }).toList()),
-        floatingActionButton: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-          FloatingActionButton(
-            onPressed: appState.list.length == 1
-                ? null
-                : () {
-                    appState.handleRemoveItem();
-                  },
-            tooltip: 'Decrease Counter',
-            child: Icon(Icons.remove),
-          ),
-          SizedBox(
-            height: 12,
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              appState.handleAddItem();
-            },
-            child: Icon(Icons.add),
-            tooltip: 'Increment Counter',
-          ),
-        ]));
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text('没有任何样式'),
+        Text(
+          '居中，字号，颜色，粗细',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, color: Colors.cyan, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          '下划线 TextDecoration.underline',
+          style: TextStyle(decoration: TextDecoration.underline),
+        ),
+        Text(
+          '删除线 TextDecoration.lineThrough',
+          textAlign: TextAlign.center,
+          style: TextStyle(decoration: TextDecoration.lineThrough),
+        ),
+        Text(
+          '上划线 TextDecoration.overline',
+          style: TextStyle(decoration: TextDecoration.overline),
+        ),
+        SizedBox(height: 24),
+        Text.rich(TextSpan(text: '富文本 Text.rich(TextSpan)', children: [
+          TextSpan(text: '富文本 cyan', style: TextStyle(color: Colors.cyan, fontSize: 22)),
+          TextSpan(text: '富文本 deepPurple', style: TextStyle(color: Colors.deepPurple, fontSize: 18)),
+          TextSpan(text: '富文本 green', style: TextStyle(color: Colors.green, fontSize: 20)),
+        ])),
+        SizedBox(height: 24),
+        Text(List.generate(20, (index) => '文本溢出').join(''), overflow: TextOverflow.ellipsis),
+        SizedBox(height: 24),
+        Text(List.generate(40, (index) => '文本溢出').join(''), maxLines: 2, overflow: TextOverflow.ellipsis),
+      ],
+    ));
   }
 }
+
+// https://api.flutter.dev/flutter/widgets/Text-class.html
+
+// Text
+// TextSpan
+// TextStyle
+// TextAlign
+// TextOverflow
+// TextDecoration
+// DefaultTextStyle
+// TextDecoration
+// TextDecorationStyle
+
+// The element type 'TextSpan' can't be assigned to the list type 'Widget'.
